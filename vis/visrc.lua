a82a7cec670c454b9cee188e483e8cea2762e664
João Paulo G. Garcia <joaopauloggarcia@gmail.com>
2021-03-05 20:45:29 -0300

    atualização

---
+++ vis/visrc.lua
@@ -0,0 +1,15 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command('set theme zenburn_mod')
+	vis:command('set autoindent')
+	vis:command('set ignorecase')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Your per window configuration options e.g.
+	vis:command('set relativenumbers')
+	vis:command('set cursorline')
+end)
